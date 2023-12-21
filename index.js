3bb06d8e9d74a3c4edb9ddcf14683f76ae8fb64c
onryldzz <onr06yildiz@gmail.com>
2023-12-21 11:14:05 +0300

    updated gitignore

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
