edcc208cbf5cb15fe4c7f21021c5588cb7c63e8d
onryldzz <onr06yildiz@gmail.com>
2024-01-01 22:40:35 +0300

    phonebook updated

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -110,17 +120,48 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response,next) => {
+  Person.findById(request.params.id)
+  .then (person => {
+    if(person) {
+      response.json(person)
+    }else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
+  
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response,next) => {
   Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(err => next(err))
 })
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
